e9a6624bae26a6d00361245fc93b8753f1ad9845
Seliaste <aena.aria@protonmail.com>
2023-04-07 00:04:56 +0200

    Added and tested pathfinding using dijkstra

--- Tests/test_search_algorithms.py
+++ Tests/test_search_algorithms.py
@@ -1,11 +1,11 @@
 from unittest import TestCase
 
 from POG.Nodes import Node
-from POG.SearchAlgorithms import dijkstra
+from POG.SearchAlgorithms import dijkstra_distances, dijkstra_pathfinding
 
 
 class Test(TestCase):
-    def test_dijsktra(self):
+    def test_dijkstra(self):
         a = Node("A")
         b = Node("B")
         c = Node("C")
@@ -20,10 +20,49 @@ class Test(TestCase):
         d.add(c, 5)
         e.add(b, 7)
 
-        self.assertEqual(dijkstra(a), {a: 0, d: 2, e: 3, c: 7, b: 10})
+        self.assertEqual(dijkstra_distances(a), {a: 0, d: 2, e: 3, c: 7, b: 10})
 
-    def test_dijsktra_small(self):
+    def test_dijkstra_small(self):
         a = Node("A")
 
-        self.assertEqual(dijkstra(a), {a: 0})
+        self.assertEqual(dijkstra_distances(a), {a: 0})
 
+    def test_dijkstra_path(self):
+        a = Node("A")
+        b = Node("B")
+        a.add(b)
+
+        self.assertEqual(dijkstra_pathfinding(a, b), [a, b])
+
+    def test_dijkstra_path_long(self):
+        a = Node("A")
+        b = Node("B")
+        c = Node("C")
+        d = Node("D")
+        e = Node("E")
+
+        a.add(d, 2)
+        a.add(e, 3)
+        b.add(a, 11)
+        c.add(e, 8)
+        c.add(a, 1)
+        d.add(c, 5)
+        e.add(b, 7)
+
+        self.assertEqual(dijkstra_pathfinding(a, b), [a, e, b])
+
+    def test_dijkstra_path_impossible(self):
+        a = Node("A")
+        b = Node("B")
+        c = Node("C")
+        d = Node("D")
+        e = Node("E")
+
+        a.add(d, 2)
+        a.add(e, 3)
+        b.add(a, 11)
+        c.add(e, 8)
+        c.add(a, 1)
+        d.add(c, 5)
+
+        self.assertEqual(dijkstra_pathfinding(a, b), [])
--- src/POG/SearchAlgorithms.py
+++ src/POG/SearchAlgorithms.py
@@ -1,19 +1,49 @@
 from POG.Nodes import Node
 
 
-def dijkstra(n: Node):
-    """Returns a dictionary with all accessible nodes and the cost to get to them."""
-    costs = {n: 0}
+def dijkstra_distances(n: Node):
+    """Returns a dictionary with all accessible nodes and the distance to them."""
+    distance = {n: 0}
     black = []
     grey = [successor for successor in n.get_successors()]
     for successor in n.get_successors():
-        costs[successor] = n.get_successors().get_cost(successor)
+        distance[successor] = n.get_successors().get_cost(successor)
     while len(grey) != 0:
-        curr = min(grey, key=costs.get)
+        curr = min(grey, key=distance.get)
         for successor in curr.get_successors():
-            val = costs[curr] + curr.get_successors().get_cost(successor)
-            if (successor in black and val < costs[successor]) or successor not in black:
-                costs[successor] = val
+            val = distance[curr] + curr.get_successors().get_cost(successor)
+            if (successor in black and val < distance[successor]) or successor not in black:
+                distance[successor] = val
         black.append(curr)
         grey.remove(curr)
-    return costs
+    return distance
+
+
+def dijkstra_pathfinding(start: Node, end: Node):
+    """Returns a list of the nodes from start to end. If no path is found, returns an empty list"""
+    distance = {start: 0}
+    previous = {start: None}
+    black = []
+    grey = [successor for successor in start.get_successors()]
+    for successor in start.get_successors():
+        distance[successor] = start.get_successors().get_cost(successor)
+        previous[successor] = start
+    while len(grey) != 0:
+        curr = min(grey, key=distance.get)
+        for successor in curr.get_successors():
+            val = distance[curr] + curr.get_successors().get_cost(successor)
+            if (successor in black and val < distance[successor]) or successor not in black:
+                distance[successor] = val
+                previous[successor] = curr
+        black.append(curr)
+        grey.remove(curr)
+    if end not in previous.keys():
+        return []
+    # Now we reverse the previous path
+    res = []
+    u = end
+    while u is not None:
+        res.append(u)
+        u = previous[u]
+    res.reverse()
+    return res
